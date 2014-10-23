9f8e9094dddd7657b7339d40bc88752e909dedaf
Meikel Brandmeyer <mb@kotka.de>
2014-10-23 21:54:33 +0200

    Add guard predicates

--- src/main/clojure/transmuter/guard.clj
+++ src/main/clojure/transmuter/guard.clj
@@ -3,3 +3,15 @@
 (def stop (Object.))
 (def void (Object.))
 (def vacuum (Object.))
+
+(defn stop?
+  [this]
+  (identical? this stop))
+
+(defn void?
+  [this]
+  (identical? this void))
+
+(defn vacuum?
+  [this]
+  (identical? this vacuum))
